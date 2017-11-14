0faed36d67bb12b90b63cdc40feb9595652804e3
phister <nguyen1.618@gmail.com>
2017-11-14 10:41:37 +0100

    created functions to check distributions

--- README.md
+++ README.md
@@ -11,7 +11,24 @@ Files and data - BADS WS17/18 project
 5. Consider mixed interaction terms or power terms
 6. [Predicting Binary Outcomes in R](https://amunategui.github.io/binary-outcome-modeling/)
 7. KNN / SVC / Extreme Random Forests
+8. [Standardization of Categorical Variables](https://stats.stackexchange.com/questions/68077/are-categorical-variables-standardized-differently-in-penalized-regression)
+9. Make a few candidate models
 
 ## Important Dates
 
 1. 01.12.2017 - Individual assignment due
+
+## Variables
+
+1. `order_item_id` is a unique identifier of order item. doesn't matter
+2. `order_date` and `delivery_date` should be coerced into a date diff variable
+3. `item_id` is a factor. note that test and training set have different `item_id`'s. some different values
+4. `item_size` factor
+5. `item_color` probably not relevant on it's own
+6. `brand_id` is a factor. some different in test / training, test set should be scaled down
+7. `item_price` numeric, but in test set there are higher values
+8. `user_id` is a factor
+9. `user_title` is a factor but high class imbalance
+10. `user_dob` is date of birth. consider discretizing and removing bad values (impute?)
+11. `user_state` is a factor
+12. `user_reg_date` is a date. consider changing to "length of account" and "time between open and order"
--- Scripts/BADS.R
+++ Scripts/BADS.R
@@ -1,13 +1,13 @@
-########################################################################################
+#####################################################################################
 # BADS.R
 #
-########################################################################################
+#####################################################################################
 # Description:
 # BADS project
 # 
-########################################################################################
+#####################################################################################
 
-########################################################################################
+#####################################################################################
 # LOAD DATA
 rm(list = ls())
 
@@ -16,32 +16,85 @@ wd = file.path(Sys.getenv("HOME"),"/Documents/Projects/BADS_Project")
 setwd(wd)
 
 # Load packages
-needs(glmnet, caret, tidyverse, lubridate, zoo, corrplot)
+needs(glmnet, caret, 
+      tidyverse, lubridate, zoo, ggplot2, 
+      corrplot, PerformanceAnalytics)
 
 # Read data
 df.train <- read.csv("Data/BADS_WS1718_known.csv")
 df.test  <- read.csv("Data/BADS_WS1718_class.csv")
 
-########################################################################################
-# Data exploration and cleaning
+#####################################################################################
+# Data exploration
 
-clean.df <- function(df) {
-  df$order_date    <- as.Date(df$order_date, format = "%Y-%m-%d")
-  df$delivery_date <- as.Date(df$delivery_date, format = "%Y-%m-%d") # treat ? as missing
-  df$user_dob      <- as.Date(df$user_dob, format = "%Y-%m-%d") # treat 1900-11-19 and ? as missing
-  df$user_reg_date <- as.Date(df$user_reg_date, format = "%Y-%m-%d")
+# check distribution of categorical variables (and how they might differ)
+dist.check <- function(var) {
+  dist.train <- data.frame(
+    train = table(df.train[[var]])[order(table(df.train[[var]]))]
+  )
+  dist.test <- data.frame(
+    test = table(df.test[[var]])[order(table(df.test[[var]]))]
+  )
+  dister   <- dist.train %>% 
+    full_join(dist.test, by = c("train.Var1" = "test.Var1"))
+  
+  dister[is.na(dister)]    <- 0
+  dister[, 2]              <- dister[, 2]/sum(dister[, 2])
+  dister[, 3]              <- dister[, 3]/sum(dister[, 3])
+  dister$Difference        <- dister[, 3] - dister[, 2]
+  
+  names(dister$train.Var1) <- "Variable"
+  return(dister)
 }
 
-# do date.diff delivery-date - order-date as var
+# check return rates of cat variables
+return.check <- function(var) {
+  return.table <- as.data.frame(as.matrix.data.frame(
+    table(df.train[[var]], df.train$return)
+  ))
+  rownames(return.table) <- levels(df.train[[var]])
+  colnames(return.table) <- c("Keep", "Return")
+  return.table$Total     <- with(return.table, Return + Keep)
+  return.table$perc.ret  <- round(with(return.table, (Return / (Return + Keep))), 2)
+  return(return.table)
+}
+
+fxns <- list(dist.check = dist.check, return.check = return.check)
+
+item_color <- lapply(fxns, function(f) f("item_color"))
+item_size  <- lapply(fxns, function(f) f("item_size"))
+user_title <- lapply(fxns, function(f) f("user_title"))
+user_state <- lapply(fxns, function(f) f("user_state"))
+item_id    <- lapply(fxns, function(f) f("item_id"))
+
+# check distribution (numeric): item_price
+
+
+#####################################################################################
+# Data cleaning
+
+clean.df <- function(df) {
+  df$order_date    <- ymd(df$order_date)
+  df$delivery_date <- ymd(df$delivery_date) # treat ? as missing
+  df$user_dob      <- ymd(df$user_dob) # treat 1900-11-19 and ? as missing
+  df$user_reg_date <- ymd(df$user_reg_date)
+}
+
 # drop: order_item_id, item_color
 # convert: item_id, brand_id, user_id, user_title to factor
 # convert: delivery_time = date.diff(delivery_date - order_date)
 
-df.c <- clean.df(df)
 
-# compare distribution of variables 
+df.c <- clean.df(df.train)
 
-########################################################################################
+# corrplots of variables
+
+#####################################################################################
+# Model creation
+
+# try a few candidate models
+
+#####################################################################################
 # Model evaluation
 
 # ROC curve (after doing training / test set)
